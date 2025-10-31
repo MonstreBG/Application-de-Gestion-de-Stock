74ea86b063fcebbee76f27a2531275e12a5df3b9
MonstreBg <yannjasonlastardumonde@gmail.com>
2025-10-31 23:11:01 +0100

    La page d'inscription es terminer. C'est en Anglais alors vous allez sauf que m'excuser

---
+++ AppGestionStock/src/main/java/org/example/appgestionstock/HelloApplication.java
@@ -0,0 +1,58 @@
+package org.example.appgestionstock;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    // Variables pour stocker la position du clic
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginAdmin.fxml"));
+
+        // On récupère la racine du FXML
+        Parent root = fxmlLoader.load();
+
+        // Création de la scène
+        Scene scene = new Scene(root, 690, 700);
+
+        // Permet de déplacer la fenêtre en cliquant et glissant
+        root.setOnMousePressed((MouseEvent event) -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+
+            stage.setOpacity(.8);
+        });
+
+        root.setOnMouseReleased((MouseEvent event)  ->{
+            stage.setOpacity(1);
+        });
+
+
+        // On enlève le style de fenêtre par défaut (bordure + barre titre)
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        stage.setScene(scene);
+        stage.setTitle("AppGestion Stock");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ AppGestionStock/src/main/java/org/example/appgestionstock/HelloController.java
@@ -0,0 +1,64 @@
+package org.example.appgestionstock;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+public class HelloController {
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private AnchorPane admin_form;
+
+
+    @FXML
+    private AnchorPane employee_form;
+
+    @FXML
+    private TextField admin_username;
+
+    @FXML
+    private PasswordField admin_password;
+
+    @FXML
+    private Button admin_loginBtn;
+
+    @FXML
+    private Hyperlink admin_hyperlink;
+
+    @FXML
+    private TextField employee_id;
+
+    @FXML
+    private PasswordField employee_password;
+
+    @FXML
+    private Button employee_loginBtn;
+
+    @FXML
+    private Hyperlink employee_hyperLink;
+
+    public void switchForm(ActionEvent event) {
+        if(event.getSource() == admin_hyperlink) {
+            admin_form.setVisible(false);
+            employee_form.setVisible(true);
+
+        } else if (event.getSource()  == employee_hyperLink) {
+            admin_form.setVisible(true);
+            employee_form.setVisible(false);
+
+
+        }
+    }
+
+
+
+
+    public void close() {
+        System.exit(0);
+    }
+
+}
